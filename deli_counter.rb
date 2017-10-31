e91cd7358cc2516a08a2b4dcf58614e30ce0c89b
Alison Mackay <alisonmackay246@gmail.com>
2017-10-31 14:47:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,11 @@
 katz_deli = []
 number_day = 1
 
-def take_a_number(katz_deli)  
+def take_a_number(katz_deli)
   katz_deli <<  number_day
   line_num = katz_deli.length
-  puts "Welcome you are number #{line_num} in line."
-  number_day += 1 
+  puts "Welcome you are number #{number_day} in line."
+  number_day += 1
 end
 
 def line(katz_deli)
